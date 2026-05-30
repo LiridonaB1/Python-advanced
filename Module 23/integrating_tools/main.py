db8845b8b5d95f3ad7e33cba12cfe4c468d28911
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-05-30 14:49:31 +0200

    module 23

---
+++ Module 23/integrating_tools/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from model import Developer, Projects
+
+app = FastAPI()
+
+@app.post("/developers/")
+def create_developer(developer: Developer):
+    return {"message": "Developer created successfully", "developer": developer}
+
+
+@app.post("/projects/")
+def create_project(project: Projects):
+    return {"message": "Project created successfully", "project": project}
+
+
+
+@app.get("/projects/")
+def get_projects():
+     sample_project = Projects(
+         title = "Sample Project",
+         description = "This is a sample project",
+         language = ["HTML","CSS","JAVASRCIPT"],
+         lead_developer = Developer(name="John Doe",experience=5)
+     )
+
+     return {"projects": [sample_project]}
